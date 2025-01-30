2e653ccd89d0ac20ee095696e943e5b302734b4d
Marcin Wilk <marcin.wilk.503@gmail.com>
2025-01-30 20:17:07 +0900

    restaurant page project initialized

---
+++ restaurant-page/src/homepage.js
@@ -0,0 +1,9 @@
+export function renderHomepage(homepageContentDiv) {
+
+    const container = document.getElementById(homepageContentDiv);
+
+    container.innerHTML =`
+    <h1>SIEMA</h1>
+    `;
+};
+
---
+++ restaurant-page/src/index.js
@@ -0,0 +1,8 @@
+import "./styles.css";
+import { renderHomepage } from "./homepage";
+
+console.log("Siema");
+
+document.addEventListener("DOMContentLoaded", () => {
+    renderHomepage("content");
+});
