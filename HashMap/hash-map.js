e964bedcb800cdf831336b65ec5eee31c3c0ccac
Marcin Wilk <marcin.wilk.503@gmail.com>
2025-03-04 14:36:59 +0900

    initial commit for hash map

---
+++ HashMap/hash-map.js
@@ -0,0 +1,17 @@
+class HashMap {
+    constructor() {
+        this.loadFactor = 10;
+        this.capacity = 15;
+    }
+
+    hash(key) {
+        let hashCode = 0;
+           
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+          hashCode = primeNumber * hashCode + key.charCodeAt(i);
+        }
+     
+        return hashCode;
+      } 
+}
